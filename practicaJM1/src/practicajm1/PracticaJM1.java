317e2938d57db755fb8c8baf08f16470ae0d5850
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-10-02 17:25:00 +0200

    Change autors in the comments of javadoc

--- practicaJM1/src/practicajm1/PracticaJM1.java
+++ practicaJM1/src/practicajm1/PracticaJM1.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practicajm1;
 
 /**
  *
- * @author Alumnot
+ * @author Daniel Marcos, Alvaro Gomez
  */
 public class PracticaJM1 {
 
@@ -15,7 +10,7 @@ public class PracticaJM1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
