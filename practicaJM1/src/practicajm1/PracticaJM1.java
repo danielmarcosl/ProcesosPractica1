b661f23a0163acb806dfcbcdfe5e771fa583e143
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-10-07 14:05:41 +0200

    Practica1 done

--- practicaJM1/src/practicajm1/PracticaJM1.java
+++ practicaJM1/src/practicajm1/PracticaJM1.java
@@ -6,15 +6,50 @@ import java.util.*;
 /**
  *
  * @author Daniel Marcos, Alvaro Gomez
+ * @version 1
  */
 public class PracticaJM1 {
 
+    // Variable global con el numero de usuarios
     static int nUsuarios = 6;
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // por ejemplo
+    public static void main(String args[]) throws InterruptedException {
+
+        try {
+            // Creamos un array de procesos
+            Process[] proceso = new Process[nUsuarios]; // Array de procesos
+
+            // Bucle para crear los 6 procesos, cada uno guardará en un documento el contenido de la carpeta de cada usuario
+            for (int i = 0; i < nUsuarios; i++) {
+                ProcessBuilder pb = new ProcessBuilder("CMD", "/C", "dir /b D:\\Libraries\\Documents\\Usuarios\\Usuario" + (i + 1));
+
+                pb.redirectOutput(new File("D:\\Libraries\\Documents\\Usuarios\\usuario" + (i + 1) + ".txt"));
+                pb.redirectError(new File("D:\\Libraries\\Documents\\Usuarios\\error.txt"));
+                proceso[i] = pb.start();
+            }
+
+            // Bucle para esperar que acaben los 6 procesos creados
+            for (int i = 0; i < nUsuarios; i++) {
+                int rc = proceso[i].waitFor();
+            }
+
+            // Bucle para imprimir por pantalla el fichero de cada usuario
+            for (int j = 0; j < nUsuarios; j++) {
+                BufferedReader leer = new BufferedReader(new FileReader("D:\\Libraries\\Documents\\Usuarios\\usuario" + (j + 1) + ".txt"));
+                String linea = null;
+
+                System.out.println("El Usuario" + (j + 1) + " contiene en su carpeta los siguientes archivos:");
+                while ((linea = leer.readLine()) != null) {
+                    System.out.println(linea);
+                }
+
+                leer.close();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
     }
-    
 }
